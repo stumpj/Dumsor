ca940b904edf9d9703455e824cfc404f2fcfa355
bfjellman <fjellman@uw.edu>
2015-09-29 12:40:30 -0700

    Added Facebook Login -

--- app/src/main/java/dumsor/org/dumsor/LoginActivity.java
+++ app/src/main/java/dumsor/org/dumsor/LoginActivity.java
@@ -4,13 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
@@ -27,18 +33,65 @@ public class LoginActivity extends Activity {
     private static final String TWITTER_KEY = "ZDBXQCMOBFN6sw4kC5zUxbWPr";
     private static final String TWITTER_SECRET = "i5xSWYArLXTLQjJH6GEstGMcHcmqJVPJcVW3VXHzsWSqxp1Qv3";
 
+    private CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
 
-
         setContentView(R.layout.activity_login);
-        //setContentView(R.layout.test_layout);
 
         final SharedPreferences sharedPreferences = this.getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+
+        if (sharedPreferences.contains("login") && sharedPreferences.contains("uid"))
+        {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        //Facebook Login
+
+        LoginButton facebookButton = (LoginButton) findViewById(R.id.facebook_button);
+        facebookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                Toast.makeText(LoginActivity.this, "Facebook Login Success.", Toast.LENGTH_SHORT).show();
+
+                sharedPreferences
+                        .edit()
+                        .putString("login", "Facebook")
+                        .putString("uid", loginResult.getAccessToken().getUserId())
+                        .apply();
+
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+            @Override
+            public void onCancel()
+            {
+                Toast.makeText(getApplicationContext(), "Facebook Login Cancelled.",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception)
+            {
+                Toast.makeText(getApplicationContext(), "Facebook Login Failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         //Twitter Login
         TwitterLoginButton twitterLogin = (TwitterLoginButton) findViewById(R.id.twitter_button);
@@ -61,8 +114,6 @@ public class LoginActivity extends Activity {
                 Toast.makeText(LoginActivity.this, "Twitter Login Failed.", Toast.LENGTH_SHORT).show();
             }
         });
-
-
 
         //Guest Login
         Button guestLogin = (Button) findViewById(R.id.guest_button);
@@ -92,6 +143,7 @@ public class LoginActivity extends Activity {
 
         // Pass the activity result to the login button.
         twitterLogin.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
